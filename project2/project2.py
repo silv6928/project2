cf53253a1606812db9138dee16839ce211090147
Tony_Silva <tonysilva.ou@gmail.com>
2017-04-18 15:11:33 -0500

    getting the contents of each cluster

--- project2/project2.py
+++ project2/project2.py
@@ -3,11 +3,13 @@
 
 import numpy as np
 import pandas as pd
+import matplotlib
+matplotlib.use('TkAgg')
+from matplotlib import pyplot as plt
+from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
 
 
-from sklearn import metrics
-from scipy.spatial.distance import squareform
-from scipy.spatial.distance import pdist
+np.set_printoptions(precision=7, suppress=True)
 
 df = pd.read_csv("cluster_set.csv", header=0, sep=',', names=['a', 'b', 'dist'])
 df.set_index(["a", "b"])
@@ -15,7 +17,6 @@ df.set_index(["a", "b"])
 a = list(set(df["a"]))
 b = list(set(df["b"]))
 total = list(sorted(set(a + b)))
-
 sim = np.ones((len(total), len(total)))
 sim = pd.DataFrame(sim, index=total, columns=total)
 for index, row in df.iterrows():
@@ -23,10 +24,30 @@ for index, row in df.iterrows():
 for i in range(len(sim)):
     sim.iloc[i][i] = 0
 
-print(sim.head())
+
+clust = linkage(sim.values, 'ward')
+
+plt.title('Hierarchical Clustering Dendrogram (truncated)')
+plt.xlabel('cluster size')
+plt.ylabel('distance')
+dendrogram(
+    clust,
+    truncate_mode='lastp',
+    p=20,
+    leaf_rotation=90.,
+    leaf_font_size=12.,
+    show_contracted=True,
+)
+
+plt.show()
 
 
+clusters = fcluster(clust, 20, criterion='distance')
+cluster_id = list(zip(sim.index.tolist(), clusters.tolist()))
 
 
+c = [[]] * len(set(clusters.tolist()))
+for i in cluster_id:
+    c[i[1] - 1].append(i[0])
 
 

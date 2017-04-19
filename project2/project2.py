56bca4ee83672714c3bf4c510ce7ab0ffd9a0bc7
Tony_Silva <tonysilva.ou@gmail.com>
2017-04-19 11:20:55 -0500

    printing of clusters is done

--- project2/project2.py
+++ project2/project2.py
@@ -8,26 +8,43 @@ matplotlib.use('TkAgg')
 from matplotlib import pyplot as plt
 from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
 
-
+# Set the precision to be 7 decimal points
 np.set_printoptions(precision=7, suppress=True)
-
+# Read in the CSV data. Note: This csv file is the original file minus the first four rows
 df = pd.read_csv("cluster_set.csv", header=0, sep=',', names=['a', 'b', 'dist'])
 df.set_index(["a", "b"])
 
+# Create a list of all of the ingredients
 a = list(set(df["a"]))
 b = list(set(df["b"]))
 total = list(sorted(set(a + b)))
-sim = np.ones((len(total), len(total)))
-sim = pd.DataFrame(sim, index=total, columns=total)
+# From the list of all the ingredients, create a distance matrix
+dist = np.ones((len(total), len(total)))
+dist = pd.DataFrame(dist, index=total, columns=total)
+# Fill in the distance matrix based on the number of shared compounds between all ingredients
+# If the ingredients contain a 1, then the two ingredients do not share an compounds
+# The closer the metric is to 0 the 'closer' the items are together
 for index, row in df.iterrows():
-    sim[row['a']][row['b']] = (1/(1+int(row['dist'])))
-for i in range(len(sim)):
-    sim.iloc[i][i] = 0
+    dist[row['a']][row['b']] = (1/(1+int(row['dist'])))
+for i in range(len(dist)):
+    dist.iloc[i][i] = 0
+
+# Perform the clustering using Ward method
+clust = linkage(dist.values, 'ward')
+# Getting the cluster assignments based on the maximum distance of 20.
+clusters = fcluster(clust, 30, criterion='distance')
+cluster_id = list(zip(dist.index.tolist(), clusters.tolist()))
+
+# Get the contents of each cluster
+c = [[] for i in range(len(set(clusters.tolist())))]
+for i in cluster_id:
+    c[i[1] - 1].append(i[0])
+# Print the cluster contents
+for i in c:
+    print(i)
 
 
-clust = linkage(sim.values, 'ward')
-
-# Print the visualization, need to change to a function
+# Print the visualization
 plt.title('Hierarchical Clustering Dendrogram (truncated)')
 plt.xlabel('cluster size')
 plt.ylabel('distance')
@@ -39,16 +56,7 @@ dendrogram(
     leaf_font_size=12.,
     show_contracted=True,
 )
-
 plt.show()
 
-# Getting the cluster assignments based on the maximum distance of 20.
-clusters = fcluster(clust, 20, criterion='distance')
-cluster_id = list(zip(sim.index.tolist(), clusters.tolist()))
-
-# Get the contents of each cluster
-c = [[]] * len(set(clusters.tolist()))
-for i in cluster_id:
-    c[i[1] - 1].append(i[0])
 
 

c7ab12830e6f9e0c83fb168a226f4c868b9211bb
Tony_Silva <tonysilva.ou@gmail.com>
2017-04-30 16:03:52 -0500

    final phase1

---
+++ main.py
@@ -0,0 +1,13 @@
+# Tony Silva
+# Main file.
+import project2
+from project2 import project2
+
+
+def main():
+
+    project2.runPhase1()
+
+
+if __name__ == "__main__":
+    main()
--- project2/project2phase2.py
+++ project2/project2phase2.py
@@ -6,7 +6,11 @@ import numpy as np
 import pandas as pd
 import random
 import sklearn
-
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier as KNN
+from sklearn.naive_bayes import MultinomialNB as NB
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn import svm
 # 39774 different Receipes
 # 6714 total unique ingredients
 # Salt is the most common, followed by onions, and olive oil
@@ -32,12 +36,12 @@ for i in df.ingredients:
 
 all_ingredients = nltk.FreqDist(all_ingredients)
 
-# Get the 4000 most common ingredients
-ingredient_features = [i[0] for i in all_ingredients.most_common()[:1500]]
+# Get the 1500 most common ingredients
+ingredient_features = [i[0] for i in all_ingredients.most_common()[:2200]]
 # Need to add the target value to the feature set for our algorithms to work
 
 
-# Helper function taken from sentdex videos
+# Helper function taken from sentdex videos in unit 8
 def find_features(document):
     ingredients = set(document)
     features = {}
@@ -45,11 +49,29 @@ def find_features(document):
             features[i] = (i in ingredients)
     return features
 
-featuresets = [find_features(row[0])  for row in documents]
+featuresets = [find_features(row[0]) for row in documents]
 
 featuresets = pd.DataFrame(featuresets)
 featuresets = pd.concat([df["cuisine"], featuresets], axis=1)
+predictors = list(featuresets.columns.values)[1:]
+target = list(featuresets.columns.values)[0]
+train, test = train_test_split(featuresets, test_size=0.3, random_state=44, stratify=featuresets["cuisine"].tolist())
 
-print(featuresets.head())
 
+nb = NB()
+nb.fit(train[predictors], train[target])
+predictions = nb.predict(test[predictors])
+actual = test[target].tolist()
 
+print(nb.score(test[predictors], test[target]))
+print(classification_report(actual, predictions))
+print(accuracy_score(actual, predictions))
+
+clf = svm.SVC(decision_function_shape='ovo')
+clf.fit(train[predictors], train[target])
+predictions = clf.predict(test[predictors])
+actual = test[target].tolist()
+
+print(clf.score(test[predictors], test[target]))
+print(classification_report(actual, predictions))
+print(accuracy_score(actual, predictions))

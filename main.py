d99b9f211b96f6826e275f3d1da32fbda2c1e78c
Tony_Silva <tonysilva.ou@gmail.com>
2017-05-07 08:43:58 -0500

    final

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 # Tony Silva
+# CS 5970 - Text Analytics
 # Main file.
 import project2
 from project2 import project2, project2phase2
 
 
 def main():
-    # Commented Out Phase 1
+
+    # Commented Out Phase 1 Assumption was to only deal with Phase 2 since Phase 1 was turned in.
     # project2.runPhase1()
     project2phase2.ui()
 
--- project2/project2phase2.py
+++ project2/project2phase2.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# Tony Silva
+# CS 5970 - Text Analytics
+# Project 2 Phase 2
+
 import nltk
-import numpy as np
 import pandas as pd
 import pickle
 import os, sys
 import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import BernoulliNB as NBB
-from sklearn.metrics import classification_report, accuracy_score
+from sklearn.metrics import classification_report
 from sklearn import svm
 from sklearn import linear_model
 from sklearn.neighbors import NearestNeighbors
+
+
 # 39774 different Receipes
 # 6714 total unique ingredients
 # Salt is the most common, followed by onions, and olive oil
@@ -28,7 +33,11 @@ from sklearn.neighbors import NearestNeighbors
 #             features[i] = (i in ingredients)
 #     return features
 
-
+# Creates the data set that was used for generation of the machine learning models
+# as well as the usage of K-Nearest Neighbors search. The data set is formatted in a pandas dataframe
+# where each column is an ingredient and each row is a recipe from the yummly.json file provided for the project
+# the contents of each cell of the data frame is True or False. True means the ingredient designated as that column is
+# contained in the row's recipe. False means the ingredient designated as that column is not contained in the recipe
 def create_feature_set():
     dir_path = os.path.dirname(sys.argv[0])
     df = pd.read_json(dir_path + "/data/yummly.json")
@@ -54,6 +63,9 @@ def create_feature_set():
     return featuresets
 
 
+# This function trains a naive bayes model on the featureset that was generated in the create_feature_set module.
+# It trains a model on 70% training test, and tests accuracy on the other 30% test data.
+# Accuracy is around 73%
 def naive_bayes_model(featuresets):
     predictors = list(featuresets.columns.values)[1:]
     target = list(featuresets.columns.values)[0]
@@ -66,6 +78,11 @@ def naive_bayes_model(featuresets):
     print(nb.score(test[predictors], test[target]))
 
 
+# This function trains a support vector machine learning model on the featureset data that was generated.
+# It trains a model on 70% training test, and tests accuracy on the other 30% test data.
+# Accuracy is around 75%.
+# This model was used in making predictions in the Cuisine Prediction System. The model was saved as a pickle file
+# The pickle file is then used to quickly pull in the trained classifier and generate predictions.
 def support_vector_model(featuresets):
     predictors = list(featuresets.columns.values)[1:]
     target = list(featuresets.columns.values)[0]
@@ -80,6 +97,9 @@ def support_vector_model(featuresets):
     pickle.dump(clf, open(filename, 'wb'))
 
 
+# This function trains a logistic regression model on the featureset data that was generated.
+# It trains a model on 70% training test, and tests accuracy on the other 30% test data.
+# Accuracy was about 75%-77%.
 def log_reg_model(featuresets):
     predictors = list(featuresets.columns.values)[1:]
     target = list(featuresets.columns.values)[0]
@@ -94,21 +114,17 @@ def log_reg_model(featuresets):
     pickle.dump(clf, open(filename, 'wb'))
 
 
-# Returns the loaded classifier from the pickle file
+# Helper function
+# Returns the loaded classifier from the pickle file.
 def load_model():
     dir_path = os.path.dirname(sys.argv[0])
     filename = "/models/finalized_model.sav"
     clf = pickle.load(open(dir_path + filename, 'rb'))
     return clf
 
-# Make a prediction on a single instance
-# Used to test predictions!
-# temp = featuresets.loc[1, list(featuresets.columns.values)[1:]]
-# temp = temp.values.reshape(1, -1)
-# print(clf.predict(temp))
-# print(featuresets.loc[1, list(featuresets.columns.values)[0]])
 
-
+# This grabs the list of ingredient features from the data set
+# The return value is used to generate the user's ingredients into a feature vector
 def grab_df_ingredients():
     dir_path = os.path.dirname(sys.argv[0])
     df = pd.read_json(dir_path + "/data/yummly.json")
@@ -123,6 +139,8 @@ def grab_df_ingredients():
     return ingredient_features
 
 
+# This actually takes the users input and all of the ingredient features and generates a single
+# feature vector for the user's input. So that the single vector can have a prediction generated for its cuisine
 def user_input_to_feature(document, ingredient_features):
     ingredients = set(document)
     feature = {}
@@ -131,6 +149,7 @@ def user_input_to_feature(document, ingredient_features):
     return feature
 
 
+# Generate a K-Nearest Neighbor search over the data set. It will return the Recipe ID's of the two nearest neighbors.
 def find_nearest_neighbors(temp, df):
     predictors = list(df.columns.values)[1:]
     knn = NearestNeighbors(n_neighbors=2, algorithm='auto',).fit(df[predictors])
@@ -138,6 +157,8 @@ def find_nearest_neighbors(temp, df):
     return indices[0][0], indices[0][1]
 
 
+# This is the command line User Interface that executes phase 2. This makes calls the functions above to pull in the
+# the data, request ingredients to make predictions on, predict the cuisine, and find the 2 nearest neighbors.
 def ui():
     print("Welcome to the Cuisine Prediction System!")
     print("Please wait as the system collects the data. (This takes 1-2 minutes)")
